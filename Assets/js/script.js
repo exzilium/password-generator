3b97a83418133000febe85c11cb9f0acf5f6e5be
Casey Dwyer <caseyjdwyer@gmail.com>
2022-11-29 21:36:47 -0800

    Added logic to prevent errors if user selects no character options, if they choose a non-numeric value for character count. Fixed but where workflow cancelation didn't end existing function before starting another.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -103,7 +103,7 @@ var specialChars = [
   "~",
 ];
 
-// Global "final array" variable to use in random password generator
+// Global "final array" variable to concat original arrays above depending on user selections and for use in random password generator
 
 var finalUserArray;
 
@@ -111,116 +111,156 @@ var finalUserArray;
 
 generatePassword = function () {
   // Wecome instructions
-  window.alert("Let's create a password");
+  window.alert(
+    "Hello, there! I will help you create a password using characters of your choosing.\n\nClick OK to continue."
+  );
 
-  // User Chooses number of characters to use when generating pw
+  // User Chooses number of characters to use when generating password
 
   var userCharCount = window.prompt(
     "How long would you like your password to be? (Choose between 8 to 128 characters)"
   );
   // check to see if user selected between 8-128 characters
 
-  if (userCharCount < 8 || userCharCount > 128) {
-    window.alert("Please choose a number between 8 and 128");
-    generatePassword();
-  } else {
-    window.alert("You chose: " + userCharCount);
+  if (userCharCount < 8 || userCharCount > 128 || isNaN(userCharCount)) {
+    // If a number is outside the 8-128 or not a number, re-start workflow.
     console.log(userCharCount);
+    var errorContinue = window.confirm(
+      "Please choose a number between 8 and 128.\nWould you like to try again?"
+    );
+    if (errorContinue) {
+      generatePassword();
+      return;
+    } else {
+      // If user cancels, exit the function
+      window.alert("Understandable. Have a nice day. ✌️");
+
+      return "Try Again";
+    }
+
+    // If user entry is valid and user clicks OK, continue the workflow
+  } else {
+    window.alert(
+      "You chose: " +
+        userCharCount +
+        ". One of my favorite numbers!\nLet's choose what type of characters you'd like in your password."
+    );
   }
 
   // Confirm which types of characters to use when generating password, then concat true values to form a final array to randomly choose from
 
-  var userUpperCase = window.confirm("Would you like UPPERCASE letters?");
+  var userUpperCase = window.confirm(
+    "Would you like to use 'UPPERCASE' letters?"
+  );
 
   // if any of the confirm dialogus are true, update global final user array to include uppercase array values
 
   if (userUpperCase) {
     finalUserArray = upperCase;
     console.log(finalUserArray);
+    console.log(userCharCount);
   }
 
-  var userLowerCase = window.confirm("Would you like lowercase letters?");
+  var userLowerCase = window.confirm(
+    "Would you like to use 'lowercase' letters?"
+  );
 
   // if true, concat new array
 
   if (userLowerCase) {
     finalUserArray = finalUserArray.concat(lowerCase);
     console.log(finalUserArray);
+    console.log(userCharCount);
   }
 
-  var userNumbers09 = window.confirm("Would you like to use numbers?");
+  var userNumbers09 = window.confirm(
+    "Would you like to use numbers?\nI'll try to pick a few lucky ones."
+  );
 
   // if true, concat new array
 
   if (userNumbers09) {
     finalUserArray = finalUserArray.concat(numbers09);
     console.log(finalUserArray);
+    console.log(userCharCount);
   }
 
   var userSpecialChars = window.confirm(
-    "Do you want your password to be extra special (and secure) by using special characters?"
+    "Do you want your password to be extra special (and extra secure) by using *special* characters?"
   );
 
-  // if true, concat new array
+  // if all values are false, prompt to try again
+
+  if (!userUpperCase && !userLowerCase && !userNumbers09 && !userSpecialChars) {
+    var errorContinue = window.confirm(
+      "You have to let me use some type of characters!\nWould you like to try again?"
+    );
+
+    // If user wants to continue, restart the function
+    if (errorContinue) {
+      generatePassword();
+      return;
+    } else {
+      // If user cancels, exit the function
+      window.alert("Understandable. Have a nice day. ✌️");
+
+      return "Try Again";
+    }
+  }
+  // if specialChar selection is true, concat new array
 
   if (userSpecialChars) {
     finalUserArray = finalUserArray.concat(specialChars);
     console.log(finalUserArray);
+    console.log(userCharCount);
   }
 
-  // End of confirmation dialogues. Confirm selections to user.
+  // End of confirmation dialogues for choosing character types. Confirm selections to user.
 
   var userContinue = window.confirm(
-    "Thank you. Here's what I have so far - Number of Characters: " +
+    "Thank you. Here's what I have so far -\nNumber of Characters: " +
       userCharCount +
-      ", Use Uppercase: " +
+      "\nUse Uppercase: " +
       userUpperCase +
-      ", Use Lowercase: " +
+      "\nUse Lowercase: " +
       userLowerCase +
-      ", Use Numbers: " +
+      "\nUse Numbers: " +
       userNumbers09 +
-      ", Use Specials: " +
+      "\nUse Special Characters: " +
       userSpecialChars +
-      " If this looks good, click OK!"
+      "\n\nIf this looks good, click OK!"
   );
 
-  console.log(
-    "Uppercase: " +
-      userUpperCase +
-      ", Lowercase: " +
-      userLowerCase +
-      ", Numbers: " +
-      userNumbers09 +
-      ", Specials: " +
-      userSpecialChars
-  );
-
-  // If user clicks OK to continue, generate a password. Else, exit the function.
+  // If user clicks OK to continue, generate a password. Else, exit the function
 
   if (userContinue) {
-    // variable to collect the new password from the random number generator and array values
+    // variable to collect the new password from the random number generator and array value picker below
 
     var newPassword = "";
 
-    // generate a random number for the length of the array
+    // generate a random number for the length of the full concatenated password array
 
     for (let i = 0; i < userCharCount; i++) {
       var randomNumber = Math.floor(Math.random() * finalUserArray.length);
       console.log(randomNumber);
 
-      // That random number will correspond to a character from the array. Take that character and add it to the generated password
+      // The above random number will correspond to the index of a character within the array. Take that character and add it to the new generated password variable
 
-      console.log(finalUserArray[randomNumber]);
       newPassword += finalUserArray[randomNumber];
     }
 
-    // Return the new value so that it sets the value of generatedPassword() to equal the new password
+    // Return the new value so that it sets the value of generatedPassword() to equal the newly generated password
 
     console.log(newPassword);
     return newPassword;
-
-    // end function on user cancel
   } else {
-    return;
+    // Prompt user to try again or cancel
+    var errorContinue = window.confirm("Would you like to try again?");
+    if (errorContinue) {
+      generatePassword();
+      return;
+    } else {
+      return "Try again";
+    }
   }
 };
